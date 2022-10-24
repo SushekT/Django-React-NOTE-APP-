daf24084f20ac57e5c0de277f08d70876fcb9981
Sushek Tamrakar <sushek69@gmail.com>
2022-10-24 16:53:40 +0545

    necessary file added

---
+++ precache-manifest.80acf5bd798d282d4225364bea51eecc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3e1a643ed171b80ce4f",
+    "url": "/static/css/main.994f195a.chunk.css"
+  },
+  {
+    "revision": "f3e1a643ed171b80ce4f",
+    "url": "/static/js/main.f3e1a643.chunk.js"
+  },
+  {
+    "revision": "b801e35b9114c62a4ffd",
+    "url": "/static/js/1.b801e35b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f6a9792d28b2fc5c6285a58ede27c63e",
+    "url": "/static/media/add.f6a9792d.svg"
+  },
+  {
+    "revision": "b3c2e7e4179fcf28e68c0053e89bd1e8",
+    "url": "/static/media/chevron-left.b3c2e7e4.svg"
+  },
+  {
+    "revision": "ba156c18f71615edcb1992184f519b76",
+    "url": "/static/media/sun.ba156c18.svg"
+  },
+  {
+    "revision": "ca9b7494d861f9f3cc559a7d0ffab864",
+    "url": "/static/media/moon.ca9b7494.svg"
+  },
+  {
+    "revision": "bba867d976edf56c2708acdda1c49d4b",
+    "url": "/index.html"
+  }
+];
